5938e53dd21a770b167596f116a38d9aa871bf91
CareerSource Central Florida <63732969+careersourcecf@users.noreply.github.com>
2021-06-18 18:12:15 +0000

    searchable apps

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@
 // created 2021-06-17
 const APP = {
     meta: {
-        quickGuides: guidesData
+        quickGuides: guidesData,
+        apps: applications
     },
     element: {
         search: {
             dropdown: {
                 container: document.querySelector('.search-dropdown'),
-                list: document.querySelector('.search-dropdown ul'),
+                list: document.querySelector('.search-dropdown ul.itemList'),
+                appList: document.querySelector('.search-dropdown ul.appList')
             },
             input: document.querySelector('.search input')
         }
@@ -16,10 +18,15 @@ const APP = {
     exec: {
         onLoad: () => {
             console.log('/js/script.js connected.');
+            // add apps to guides
+            for(let guide of APP.meta.quickGuides){
+                guide.appData = APP.meta.apps[guide.application];
+            }
             // listeners
             APP.LISTENER.searchInputType();
             APP.LISTENER.searchInputFocus();
             APP.DOM.buildDropdown(APP.meta.quickGuides);
+
         },
         searchGuides: query => {
             let results = []
@@ -30,6 +37,17 @@ const APP = {
                 }
             }
             return results;
+        },
+        searchApps: query => {
+            let results = [];
+            if(!query){return results};
+            for(let app in APP.meta.apps){
+                let title = app.toLowerCase();
+                if(title.includes(query.toLowerCase())){
+                    results.push(APP.meta.apps[app]);
+                }
+            }
+            return results
         }
     },
     DOM: {
@@ -42,11 +60,11 @@ const APP = {
         buildDropdown: itemArr => {
             let html = ``;
             for(let item of itemArr){
-                html+= APP.DOM.createDropdownItemHMTL(item);
+                html+= APP.DOM.createDropdownItemHTML(item);
             }
             APP.element.search.dropdown.list.innerHTML = html;
         },
-        createDropdownItemHMTL: item => {
+        createDropdownItemHTML: item => {
             const types = {
                 quickGuide: `./image/paper.svg`
             }
@@ -66,12 +84,41 @@ const APP = {
                 </a>
             </li>
             `
+        },
+        createDropdownAppHTML: item => {
+            const types = {
+                quickGuide: `./image/paper.svg`
+            }
+            return `
+            <li>
+                <a target="_blank" href="${item.url}">
+                    <div class="icon-container" data-type="app">
+                        <img class="icon" src="${item.icon}">
+                    </div>
+                    <div class="link-label">
+                        <span class="link-title">${item.name}</span>
+                        <div class="link-meta">
+                        <span class="link-origin" data-origin="application">App</span>
+                        </div>
+                    </div>
+                </a>
+            </li>
+            `
+        },
+        buildAppSearchDropdown: itemArr => {
+            
+            let html = ``;
+            for(let item of itemArr){
+                html+= APP.DOM.createDropdownAppHTML(item);
+            }
+            APP.element.search.dropdown.appList.innerHTML = html;
         }
     },
     LISTENER: {
         searchInputType: () => {
             APP.element.search.input.addEventListener('input', function(){
                 APP.DOM.buildDropdown(APP.exec.searchGuides(this.value));
+                APP.DOM.buildAppSearchDropdown(APP.exec.searchApps(this.value));
                 if(this.value){
                     // show the drop down
                     APP.DOM.unHide(APP.element.search.dropdown.container)
